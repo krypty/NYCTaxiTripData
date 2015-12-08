c3102bd1118c3d056afaafe40708cbfc295e3e1e
krypty <gmarigliano93@gmail.com>
2015-12-08 17:16:55 +0100

    small tests in TripDataParser.scala

--- src/main/scala/parsers/TripDataParser.scala
+++ src/main/scala/parsers/TripDataParser.scala
@@ -17,6 +17,16 @@ object TripDataParser {
       .option("header", "true") // Use first line of all files as header
       .load(TRIP_DATA_FILENAME).registerTempTable("yolo_table")
 
+
+    val df2 = sqlContext.read
+      .format("com.databricks.spark.csv")
+      .option("header", "true") // Use first line of all files as header
+      .load(TRIP_DATA_FILENAME)
+
+    println("------------")
+    df2.rdd.filter(x => x.getString(1).equals("7CE849FEF67514F080AF80D990F7EF7F")).foreach(a => println(a.toString()))
+    println("------------")
+
     // print the csv structure
     //    df.printSchema()
 
@@ -24,14 +34,15 @@ object TripDataParser {
     //    df.groupBy("vendor_id").count().show()
 
     // or using raw sql
-    val records = sqlContext.sql( """select * from yolo_table WHERE hack_license = '7CE849FEF67514F080AF80D990F7EF7F'""").take(5)
+    sqlContext.sql( """SELECT hack_license, COUNT(medallion) as medallion_count FROM yolo_table GROUP BY hack_license, medallion ORDER BY medallion_count DESC""").show()
+
 
     // print query results
     //    sqlContext.sql( """select * from yolo_table WHERE hack_license = '7CE849FEF67514F080AF80D990F7EF7F'""").show()
 
     // get query results
-    val colNumber = 5
-    println(records.foreach(x => println(x.getString(colNumber))))
+    //    val colNumber = 5
+    //    println(records.foreach(x => println(x.getString(colNumber))))
 
   }
 }
