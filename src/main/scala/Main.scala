a69e734e85c4e95f14be1f6ac1ec11b8667cc443
krypty <gmarigliano93@gmail.com>
2015-12-29 10:51:21 +0100

    refactor Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,50 @@
+
 import com.esri.core.geometry.Point
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
 
 object Main {
   def main(args: Array[String]) {
+    // reduce spark verbosity
+    Logger.getLogger("org").setLevel(Level.WARN)
+    Logger.getLogger("akka").setLevel(Level.WARN)
+
+//    val TRIP_DATA_FILENAME = "taxidata/trip_data_1.csv"
+        val TRIP_DATA_FILENAME = "taxidata/trip_data_first10000.csv"
+
+    val conf = new SparkConf().setMaster("local[2]").setAppName("Simple Application")
+    val sc = new SparkContext(conf)
+
+    val sqlContext = new SQLContext(sc)
+
+    val df = sqlContext.read
+      .format("com.databricks.spark.csv")
+      .option("header", "true") // Use first line of all files as header
+      .load(TRIP_DATA_FILENAME).registerTempTable("tripdata_table")
 
 
     val gjParser = new GeoJsonParser("conciles.geojson")
     val regions = gjParser.parse()
-
     val regionsManager = new RegionsManager(regions)
 
-    val libertyStatue = new Point(-74.044444, 40.689167)
-    val neuchatel = new Point(6.930567, 46.990281)
+    def getRegion(longitude: Double, latitude: Double) = {
+      regionsManager.getRegion(new Point(longitude, latitude))
+    }
+    sqlContext.udf.register("getRegion", getRegion(_: Double, _: Double))
 
-    println(regionsManager.getRegion(libertyStatue))
-    println(regionsManager.getRegion(neuchatel))
+    // or using raw sql
+    sqlContext.sql( """SELECT *, getRegion(pickup_longitude, pickup_latitude) as pickup_region FROM tripdata_table """).registerTempTable("tripdata_region_table")
 
+    val sumInSecs = sqlContext.sql( """SELECT sum(trip_time_in_secs) FROM tripdata_region_table""").collect()(0)(0).asInstanceOf[Double]
+    println("sum in secs: " + sumInSecs)
+    sqlContext.sql( """SELECT pickup_region, sum(trip_time_in_secs) as total_time_in_secs FROM tripdata_region_table GROUP BY pickup_region """).registerTempTable("region_time_table")
+
+    val sumInSecsStr: String = "%.1f".format(sumInSecs)
+    val rdd = sqlContext.sql( s"""SELECT pickup_region, total_time_in_secs, (total_time_in_secs / $sumInSecsStr) * 100 as total_time_in_percent FROM region_time_table ORDER BY total_time_in_secs DESC, pickup_region ASC """).rdd
+
+    println("rdd count : " + rdd.count())
+
+    gjParser.generateJsonFile(rdd)
   }
 }
